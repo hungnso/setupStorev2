b49461c423f6165bfbb765f09a65b1402e06c3cd
hungnso <nso99tn@gmail.com>
2023-02-16 23:24:51 +0700

    fix-update

--- server/server.js
+++ server/server.js
@@ -15,6 +15,14 @@ mongoose
   .catch((err) => console.log("DB CONNECTION ERR", err));
 
 // middleware
+
+app.use((function (req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', process.env.CLIENT_DOMAIN)
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE')
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type')
+  res.setHeader('Access-Control-Allow-Credentials', true)
+  next()
+}))
 app.use(cors({
   origin: '*'
 }));
