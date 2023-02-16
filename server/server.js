d8992b6dc3a72dcc3cf72fdb57329785dba705aa
hungnso <nso99tn@gmail.com>
2023-02-16 23:06:15 +0700

    fix server

--- server/server.js
+++ server/server.js
@@ -15,15 +15,19 @@ mongoose
   .catch((err) => console.log("DB CONNECTION ERR", err));
 
 // middleware
+app.use(cors({
+  origin: '*'
+}));
 app.use(morgan("dev"));
 app.use(bodyParser.json({ limit: "2mb" }));
-app.use(cors());
 
 // routes middleware
 readdirSync("./modules").map((r) => app.use("/api", require("./modules/" + r)));
 
 // port
 const port = process.env.PORT || 9191;
+
+
 app.listen(port, (err) => {
   if (err) throw err;
   console.log(`Server is running on port ${port}`);
